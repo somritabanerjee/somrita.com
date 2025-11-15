8f5bbf7a614dff38e143262489d8b7cb51e80339
somritabanerjee <somritabanerjee@users.noreply.github.com>
2025-11-15 22:55:09 +0000

    deploy: dba5f5865b97b9016597777d2804c7005866ee4d

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "somritabanerjee/somrita.com",
+    "data-repo": "",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
